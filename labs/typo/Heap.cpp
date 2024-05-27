21d8cafcb338557c4d8d71f96d36010cf7164731
Vinod Kanigicherla <vinod.kanigicherla@gmail.com>
2024-05-27 15:34:02 -0700

    Initial commit

---
+++ labs/typo/Heap.cpp
@@ -0,0 +1,115 @@
+#include "Heap.h"
+#include <cstring>
+
+Heap::Heap(size_t capacity) : mCapacity(capacity), mCount(0) {
+  mData = new Entry[capacity];
+}
+
+Heap::Heap(const Heap &other)
+    : mCapacity(other.mCapacity), mCount(other.mCount) {
+  mData = new Entry[mCapacity];
+  std::memcpy(other.mData, mData, sizeof(*mData));
+}
+
+Heap::~Heap() { delete[] mData; }
+
+size_t Heap::capacity() const { return mCapacity; }
+
+size_t Heap::count() const { return mCount; }
+
+const Heap::Entry &Heap::lookup(size_t index) const {
+  if (index >= mCount || index < 0) {
+    throw std::out_of_range("Index out of range");
+  }
+  return mData[index];
+}
+
+Heap::Entry Heap::pop() {
+  if (mCount == 0) {
+    throw std::underflow_error("Empty heap");
+  }
+
+  Entry result = mData[0];
+  mData[0] = mData[--mCount];
+
+  size_t index = 0;
+  while (true) {
+    size_t left = (2 * index) + 1;
+    size_t right = (2 * index) + 2;
+    size_t min_index = index;
+
+    if ((left < mCount) && (mData[left].score < mData[min_index].score)) {
+      min_index = left;
+    }
+
+    if ((right < mCount) && (mData[right].score < mData[min_index].score)) {
+      min_index = right;
+    }
+
+    if (min_index != index) {
+      std::swap(mData[index], mData[min_index]);
+      index = min_index;
+    } else {
+      break;
+    }
+  }
+
+  return result;
+}
+
+Heap::Entry Heap::pushpop(const std::string &value, float score) {
+  if (mCount == 0) {
+    throw std::underflow_error("Empty heap");
+  }
+
+  if (score < mData[0].score) {
+    return {value, score};
+  }
+
+  Entry top = mData[0];
+  mData[0] = {value, score};
+
+  size_t index = 0;
+  size_t childIndex = 2 * index + 1;
+  while (childIndex < mCount) {
+    size_t rightChild = childIndex + 1;
+
+    if (rightChild < mCount &&
+        mData[rightChild].score < mData[childIndex].score) {
+      childIndex = rightChild;
+    }
+
+    if (mData[childIndex].score < mData[index].score) {
+      std::swap(mData[childIndex], mData[index]);
+      index = childIndex;
+      childIndex = 2 * index + 1;
+    } else {
+      break;
+    }
+  }
+
+  return top;
+}
+
+void Heap::push(const std::string &value, float score) {
+  if (mCapacity >= mCount) {
+    throw std::overflow_error("Filled heap");
+  }
+
+  // AGGREGATE INITIALIZATION
+  mData[mCount++] = {value, score};
+
+  // BOTTOM UP PERCOLATION
+  size_t index = mCount - 1;
+  while (index > 0) {
+    size_t parentIndex = (index - 1) / 2;
+    if (mData[index].score > mData[parentIndex].score) {
+      std::swap(mData[parentIndex], mData[index]);
+      index = parentIndex;
+    } else {
+      break;
+    }
+  }
+}
+
+const Heap::Entry &Heap::top() const { return mData[0]; }
---
+++ labs/typo/WordList.cpp
@@ -0,0 +1,59 @@
+#include "WordList.h"
+#include "Heap.h"
+#include "cmath"
+
+#include <map>
+
+bool validWord(const std::string &word) {
+  for (char c : word) {
+    if (!std::islower(c)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+WordList::WordList(std::istream &stream) {
+  std::string word;
+  while (getline(stream, word)) {
+    if (validWord(word)) {
+      mWords.push_back(word);
+    }
+  }
+}
+
+Heap WordList::correct(const std::vector<Point> &points, size_t maxcount,
+                       float cutoff) const {
+  std::map<char, std::pair<float, float>> locations = {
+      {'q', {0.0, 0.0}}, {'w', {1.0, 0.0}}, {'e', {2.0, 0.0}},
+      {'r', {3.0, 0.0}}, {'t', {4.0, 0.0}}, {'y', {5.0, 0.0}},
+      {'u', {6.0, 0.0}}, {'i', {7.0, 0.0}}, {'o', {8.0, 0.0}},
+      {'p', {9.0, 0.0}}, {'a', {0.5, 1.0}}, {'s', {1.5, 1.0}},
+      {'d', {2.5, 1.0}}, {'f', {3.5, 1.0}}, {'g', {4.5, 1.0}},
+      {'h', {5.5, 1.0}}, {'j', {6.5, 1.0}}, {'k', {7.5, 1.0}},
+      {'l', {8.5, 1.0}}, {'z', {1.0, 2.0}}, {'x', {2.0, 2.0}},
+      {'c', {3.0, 2.0}}, {'v', {4.0, 2.0}}, {'b', {5.0, 2.0}},
+      {'n', {6.0, 2.0}}, {'m', {7.0, 2.0}}};
+
+  Heap heap(maxcount);
+
+  for (const std::string &word : mWords) {
+    float score = 0;
+    for (size_t i = 0; i < points.size(); i++) {
+      std::pair<float, float> key_location = locations[word[i]];
+      float distance = sqrt(pow(key_location.first - points[i].x, 2) +
+                            pow(key_location.second - points[i].y, 2));
+      score += (1.0 / (10 * pow(distance, 2) + 1));
+    }
+
+    if (score < cutoff)
+      continue;
+    if (heap.count() < heap.capacity()) {
+      heap.push(word, score);
+    } else if (score > heap.top().score) {
+      heap.pushpop(word, score);
+    }
+  }
+
+  return heap;
+}
