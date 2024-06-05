2ebac47eec099c0fdc1a00f5cccd46f8c73816a3
Vinod Kanigicherla <vinod.kanigicherla@gmail.com>
2024-06-04 21:21:39 -0700

    Voxhop

--- labs/voxhop/VoxMap.h
+++ labs/voxhop/VoxMap.h
@@ -1,7 +1,10 @@
 #ifndef VOXMAP_H
 #define VOXMAP_H
 
+#include <bitset>
+#include <deque>
 #include <istream>
+#include <tuple>
 #include <vector>
 
 #include "Point.h"
@@ -9,13 +12,20 @@
 
 class VoxMap {
   // Member Variables
-
+  int width, depth, height;
+  std::vector<std::vector<std::bitset<4>>> map;
 
   // Helper Functions
-
+  // isValid, canStand, equals
+  bool isValid(Point p);
+  bool equals(Point p1, Point p2);
+  bool canStep(Point curr, Point step);
+  bool canStand(Point p);
+  int find_z(Point step);
+  bool is_valid_no_z(Point p);
 
 public:
-  VoxMap(std::istream& stream);
+  VoxMap(std::istream &stream);
   ~VoxMap();
 
   Route route(Point src, Point dst);
